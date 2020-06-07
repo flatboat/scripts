14051be9e333e3b234db7d6f0d13c4a54d125726
desc <presentcrane665.minecraft@gmail.com>
2020-06-07 09:23:06 -0400

    Added Dynamic Values For Hitboxes
    Added the ability to change "normal" hitboxes.
    
    Thanks to Factor#9321 for the help with this one.

--- Onetap/Hitbox On Key.js
+++ Onetap/Hitbox On Key.js
@@ -6,7 +6,7 @@
 /////////////////////////
 
 //Just saying that the script is loaded.
-Global.PrintChat("Loaded Hitbox(es) on key script by desc/jo")
+Cheat.PrintChat("Loaded Hitbox(es) on key script by desc/jo")
 
 //Storing User's Chosen Hitboxes.
 autoHitboxes = (UI.GetValue("Rage", "AUTOSNIPER", "Hitboxes"))
@@ -16,9 +16,12 @@ hvypistolHitboxes = (UI.GetValue("Rage", "HEAVY PISTOL", "Hitboxes"))
 pistolHitboxes = (UI.GetValue("Rage", "PISTOL", "Hitboxes"))
 generalHitboxes = (UI.GetValue("Rage", "GENERAL", "Hitboxes"))
 
+var original = true;
+
 //Checkboxes, Hotkeys, and Dropdowns.
 UI.AddLabel("_________________________________________")
 UI.AddCheckbox("Enable Force Hitbox On Key")
+UI.AddCheckbox("Enable Indicator")
 UI.AddHotkey("Head On Key")
 UI.AddHotkey("Body On Key")
 UI.AddMultiDropdown("Body Hitboxes", ["IGNORE ME, HEAD", "Upper Chest", "Chest", "Lower Chest", "Stomach", "Pelvis", "Legs", "Feet"]);
@@ -26,7 +29,7 @@ UI.AddLabel("_________________________________________")
 
 //Runs every frame, main function.
 function main() {
-    if (World.GetServerString() != "") { 
+    if (World.GetServerString() !== "") { 
         //Declaring Screensize
         ss = Render.GetScreenSize();
         if (UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Enable Force Hitbox On Key")) {
@@ -40,8 +43,19 @@ function main() {
             
             //Detecting if the hotkeys are pressed, if not make a small indicator in the bottom left corner.
             if (forceHeadOnly) {
-                font = Render.AddFont("Arial Black", 15, 100);
-                Render.StringCustom(10, ss[1] - 80, 0, "HEAD", [ 51, 204, 51, 255 ], font );
+                if (original === true) {
+                    autoHitboxes = (UI.GetValue("Rage", "AUTOSNIPER", "Hitboxes"))
+                    awpHitboxes = (UI.GetValue("Rage", "AWP", "Hitboxes"))
+                    scoutHitboxes = (UI.GetValue("Rage", "SCOUT", "Hitboxes"))
+                    hvypistolHitboxes = (UI.GetValue("Rage", "HEAVY PISTOL", "Hitboxes"))
+                    pistolHitboxes = (UI.GetValue("Rage", "PISTOL", "Hitboxes"))
+                    generalHitboxes = (UI.GetValue("Rage", "GENERAL", "Hitboxes"))
+                    original = false
+                }
+                if (UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Enable Indicator")) {
+                    font = Render.AddFont("Arial Black", 15, 100);
+                    Render.StringCustom(10, ss[1] - 380, 0, "HEAD", [ 51, 204, 51, 255 ], font );
+                }
                 UI.SetValue("Rage", "AUTOSNIPER", "Targeting", "Hitboxes", 1)
                 UI.SetValue("Rage", "AWP", "Hitboxes", 1)
                 UI.SetValue("Rage", "SCOUT", "Hitboxes", 1)
@@ -49,8 +63,19 @@ function main() {
                 UI.SetValue("Rage", "PISTOL", "Hitboxes", 1)
                 UI.SetValue("Rage", "GENERAL", "Hitboxes", 1)
             } else if (forceBodyOnly) {
-                font = Render.AddFont("Arial Black", 15, 100);
-                Render.StringCustom(10, ss[1] - 80, 0, "BODY", [ 51, 204, 51, 255 ], font );
+                if (original === true) {
+                    autoHitboxes = (UI.GetValue("Rage", "AUTOSNIPER", "Hitboxes"))
+                    awpHitboxes = (UI.GetValue("Rage", "AWP", "Hitboxes"))
+                    scoutHitboxes = (UI.GetValue("Rage", "SCOUT", "Hitboxes"))
+                    hvypistolHitboxes = (UI.GetValue("Rage", "HEAVY PISTOL", "Hitboxes"))
+                    pistolHitboxes = (UI.GetValue("Rage", "PISTOL", "Hitboxes"))
+                    generalHitboxes = (UI.GetValue("Rage", "GENERAL", "Hitboxes"))
+                    original = false
+                }
+                if (UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Enable Indicator")) {
+                    font = Render.AddFont("Arial Black", 15, 100);
+                    Render.StringCustom(10, ss[1] - 380, 0, "BODY", [ 51, 204, 51, 255 ], font );
+                }
                 UI.SetValue("Rage", "AUTOSNIPER", "Targeting", "Hitboxes", bodyHitboxes)
                 UI.SetValue("Rage", "AWP", "Hitboxes", bodyHitboxes)
                 UI.SetValue("Rage", "SCOUT", "Hitboxes", bodyHitboxes)
@@ -58,17 +83,22 @@ function main() {
                 UI.SetValue("Rage", "PISTOL", "Hitboxes", bodyHitboxes)
                 UI.SetValue("Rage", "GENERAL", "Hitboxes", bodyHitboxes)
             } else {
-                font = Render.AddFont("Arial Black", 15, 100);
-                Render.StringCustom(10, ss[1] - 80, 0, "NORMAL", [ 255, 64, 0, 255 ], font );
-                UI.SetValue("Rage", "AUTOSNIPER", "Hitboxes", autoHitboxes)
-                UI.SetValue("Rage", "AWP", "Hitboxes", awpHitboxes)
-                UI.SetValue("Rage", "SCOUT", "Hitboxes", scoutHitboxes)
-                UI.SetValue("Rage", "HEAVY PISTOL", "Hitboxes", hvypistolHitboxes)
-                UI.SetValue("Rage", "PISTOL", "Hitboxes", pistolHitboxes)
-                UI.SetValue("Rage", "GENERAL", "Hitboxes", generalHitboxes)
+                if (original !== true) {
+                    UI.SetValue("Rage", "AUTOSNIPER", "Hitboxes", autoHitboxes)
+                    UI.SetValue("Rage", "AWP", "Hitboxes", awpHitboxes)
+                    UI.SetValue("Rage", "SCOUT", "Hitboxes", scoutHitboxes)
+                    UI.SetValue("Rage", "HEAVY PISTOL", "Hitboxes", hvypistolHitboxes)
+                    UI.SetValue("Rage", "PISTOL", "Hitboxes", pistolHitboxes)
+                    UI.SetValue("Rage", "GENERAL", "Hitboxes", generalHitboxes)
+                    original = true;
+                }
+                if (UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Enable Indicator")) {
+                    font = Render.AddFont("Arial Black", 15, 100);
+                    Render.StringCustom(10, ss[1] - 380, 0, "NORMAL", [ 255, 64, 0, 255 ], font );
+                }
             }
         }
     }
 }
 
-Global.RegisterCallback("Draw", "main");
+Cheat.RegisterCallback("Draw", "main");
