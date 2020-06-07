170df9b9383c21e2340e0bf8f2768592f3a12a06
desc <presentcrane665.minecraft@gmail.com>
2020-06-07 09:23:53 -0400

    Changed Positions.
    Changed position of text indicators to allow better accessibility to reading chat.

--- Onetap/Indicators.js
+++ Onetap/Indicators.js
@@ -5,41 +5,59 @@
 // for onetap.su/com   //
 /////////////////////////
 
-Global.PrintChat("Loaded Indicator script by desc/jo")
+Cheat.PrintChat("Loaded Indicator script by desc/jo")
 
 UI.AddLabel("_________________________________________")
 UI.AddHotkey("Left Manual AA")
 UI.AddHotkey("Back Manual AA")
 UI.AddHotkey("Right Manual AA")
 UI.AddColorPicker("Anti-Aim Arrows Color")
-UI.AddSliderInt("Anti-Aim Arrow Scale", 0, 100)
+UI.AddSliderInt("Anti-Aim Arrow Scale", 0, 10)
+UI.AddSliderInt("Anti-Aim Arrow Distance", 0, 100)
 UI.AddLabel("_________________________________________")
 
 ss = Render.GetScreenSize();
 
-var currentAA;
+var localPlayer = Entity.GetLocalPlayer();
 
 var target = "NONE";
 function targetID(){
     target = Ragebot.GetTarget()
 }
+
 function main(){
     if (World.GetServerString() != "") { 
+        if(!Entity.IsAlive(localPlayer)) {return;}
         var tgt = Entity.GetName(target)
         if (target === 0) {tgt = "NONE"}
         font = Render.AddFont("Arial Black", 15, 100);
-        Render.StringCustom(10, ss[1] - 100, 0, "TARGET: " + tgt, [ 255, 64, 0, 255 ], font );
+        Render.StringCustom(10, ss[1] - 400, 0, "TARGET: " + tgt.toUpperCase(), [ 255, 64, 0, 255 ], font );
 
         if (UI.IsHotkeyActive("Rage", "GENERAL", "Exploits", "Doubletap")) {
-            Render.StringCustom(10, ss[1] - 120, 0, "DT", [ 51, 204, 51, 255 ], font );
+            Render.StringCustom(10, ss[1] - 420, 0, "DT", [ 51, 204, 51, 255 ], font );
         } else {
-            Render.StringCustom(10, ss[1] - 120, 0, "DT", [ 255, 64, 0, 255 ], font );
+            Render.StringCustom(10, ss[1] - 420, 0, "DT", [ 255, 64, 0, 255 ], font );
         }
 
         if (UI.IsHotkeyActive("Rage", "GENERAL", "Exploits", "Hide shots")) {
-            Render.StringCustom(10, ss[1] - 140, 0, "HIDE", [ 51, 204, 51, 255 ], font );
+            Render.StringCustom(10, ss[1] - 440, 0, "HIDE", [ 51, 204, 51, 255 ], font );
         } else {
-            Render.StringCustom(10, ss[1] - 140, 0, "HIDE", [ 255, 64, 0, 255 ], font );
+            Render.StringCustom(10, ss[1] - 440, 0, "HIDE", [ 255, 64, 0, 255 ], font );
+        }
+
+        if (UI.IsHotkeyActive("Anti-Aim", "Extra", "Fake duck")) {
+            Render.StringCustom(10, ss[1] - 480, 0, "FD", [ 51, 204, 51, 255 ], font );
+        } else {
+            Render.StringCustom(10, ss[1] - 480, 0, "FD", [ 255, 64, 0, 255 ], font );
+        }
+
+        aatype = (UI.GetValue("Anti-Aim", "Fake angles", "LBY mode"))
+        if (aatype === 0) {
+            Render.StringCustom(10, ss[1] - 500, 0, "NORMAL", [ 51, 204, 51, 255 ], font );
+        } else if (aatype === 1) {
+            Render.StringCustom(10, ss[1] - 500, 0, "OPPOSITE", [ 51, 204, 51, 255 ], font );
+        } else if (aatype === 2) {
+            Render.StringCustom(10, ss[1] - 500, 0, "SWAY", [ 51, 204, 51, 255 ], font );
         }
 
         if (UI.IsHotkeyActive("Misc", "JAVASCRIPT", "Script items", "Left Manual AA")) {
@@ -53,17 +71,18 @@ function main(){
             //Render.StringCustom(10, ss[1] - 160, 0, "RIGHT", [ 51, 204, 51, 255 ], font );
         }
 
+        var aadist = UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Anti-Aim Arrow Distance")
         var aascale = UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Anti-Aim Arrow Scale")
         var aaincolor = UI.GetColor("Misc", "JAVASCRIPT", "Script items", "Anti-Aim Arrows Color")
         if (UI.GetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset") === -90) {
-            Render.StringCustom(10, ss[1] - 160, 0, "LEFT", [ 51, 204, 51, 255 ], font );
-            Render.Polygon([[910 + aascale, 535 - aascale], [910 + aascale, 545 + aascale], [900 - aascale, 540]], [aaincolor[0], aaincolor[1], aaincolor[2], 255]);
+            Render.StringCustom(10, ss[1] - 460, 0, "LEFT", [ 51, 204, 51, 255 ], font );
+            Render.Polygon([[ss[0]/2 - 50 + aascale - aadist, ss[1]/2 - 5 - aascale], [ss[0]/2 - 50 + aascale - aadist, ss[1]/2 + 5 + aascale], [ss[0]/2 - 60  - aascale - aadist, ss[1]/2]], [aaincolor[0], aaincolor[1], aaincolor[2], 255]);
         } else if (UI.GetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset") === 0) {
-            Render.StringCustom(10, ss[1] - 160, 0, "BACK", [ 51, 204, 51, 255 ], font );
-            Render.Polygon([[955 - aascale, 590 - aascale], [965 + aascale, 590.0 - aascale], [960, 600 + aascale]], [aaincolor[0], aaincolor[1], aaincolor[2], 255]); 
+            Render.StringCustom(10, ss[1] - 460, 0, "BACK", [ 51, 204, 51, 255 ], font );
+            Render.Polygon([[ss[0]/2 - 5 - aascale, ss[1]/2 + 50 - aascale + aadist], [ss[0]/2 + 5 + aascale, ss[1]/2 + 50 - aascale + aadist], [ss[0]/2, ss[1]/2 + 60 + aascale + aadist]], [aaincolor[0], aaincolor[1], aaincolor[2], 255]); 
         } else if (UI.GetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset") === 90) {
-            Render.StringCustom(10, ss[1] - 160, 0, "RIGHT", [ 51, 204, 51, 255 ], font );
-            Render.Polygon([[1010 - aascale, 545 + aascale], [1010 - aascale, 535 - aascale], [1020 + aascale, 540]], [aaincolor[0], aaincolor[1], aaincolor[2], 255]); 
+            Render.StringCustom(10, ss[1] - 460, 0, "RIGHT", [ 51, 204, 51, 255 ], font );
+            Render.Polygon([[ss[0]/2 + 50 - aascale + aadist, ss[1]/2 + 5 + aascale], [ss[0]/2 + 50 - aascale + aadist, ss[1]/2 - 5 - aascale], [ss[0]/2 + 60 + aascale + aadist, ss[1]/2]], [aaincolor[0], aaincolor[1], aaincolor[2], 255]); 
         } 
     }
 }
